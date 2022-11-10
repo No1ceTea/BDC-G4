db9b941c4da5cb1e909985808306b22654d19dc2
Jacquot Thai <71291683+No1ceTea@users.noreply.github.com>
2022-11-10 09:35:07 +0100

    dev

--- src/app.js
+++
@@ -1,8 +0,0 @@
-function changeClass() {
-  document.getElementById('1').className = "tab tab-lg tab-bordered font-fredoka tab-active";
-  let button_class = document.getElementById('1').className;
-  document.getElementById('pId').innerHTML = "tab tab-lg tab-bordered font-fredoka tab-active" + button_class;
-}
-window.onload = function() {
-  document.getElementById("1").addEventListener('click', changeClass);
-}
